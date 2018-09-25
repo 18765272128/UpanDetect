db4a064fc961c971610d8e1b7375cf6af67a07c5
bob <1635846563@qq.com>
2018-09-25 17:58:15 +0800

    get the all the file in the dir

--- app/src/main/java/cn/wintec/bob/upandetect/BootCompletedBroadcastReceiver.java
+++ app/src/main/java/cn/wintec/bob/upandetect/BootCompletedBroadcastReceiver.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.util.Log;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by adc on 2018/9/25.
@@ -31,6 +33,7 @@ public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
     private static String Upan_Path;
     private static String APK_Dir = "Mydir";
     private static String APK_Path;
+    private List<String> FileList;
     @Override
     public void onReceive(Context context, Intent intent) {
         String maction = intent.getAction();
@@ -41,18 +44,27 @@ public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
         }else  if (maction == Upan_Mount){
             Log.e(TAG, "str = " + intent.getData().getPath());
             Upan_Path = intent.getData().getPath() + "/" + Upan_Dir;
-            APK_Path = Upan_Path + APK_Dir;
-            if(mPathIsExist(APK_Dir)){
-                Log.e(TAG,"file is exist");
-            }
+            APK_Path = Upan_Path + "/" + APK_Dir;
+            getFilesAllName(APK_Path);
         }
     }
 
-    private boolean mPathIsExist(String strFolder){
-            File file = new File(strFolder);
-            if (!file.exists())
-                return false;
-            else
-                return true;
+    private boolean getFilesAllName(String strFolder){
+        File file = new File(strFolder);
+        if (!file.exists()) {
+            Log.e(TAG, "The dir is not exist");
+            return false;
+        }
+        File[] files=file.listFiles();
+        if (files.length == 0){
+            Log.e(TAG, "The dir is empty");
+            return false;
+        }
+        FileList = new ArrayList<>();
+        for(int i = 0; i<files.length; i++){
+            FileList.add(files[i].getAbsolutePath());
+        }
+
+        return true;
     }
 }
